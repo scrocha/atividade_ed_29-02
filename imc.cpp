28176ee49300546149821b8f74abd60617fefb4e
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2024-02-29 19:30:34 -0300

    documentei o calculador de imc

--- imc.cpp
+++ imc.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 float fCalcularImc(float fMassa, float fAltura)
 {
+    // Função que realiza o cálculo básico do IMC, sendo ele massa sob altura ao quadrado.
 
     float fImc = fMassa / (fAltura*fAltura);
 
@@ -14,6 +15,8 @@ float fCalcularImc(float fMassa, float fAltura)
 
 string sVerificarImc(float fImc)
 {
+    // Função que retorna a classificação com base no IMC inserido utilizando
+    // if, else if e else.
 
     if (fImc < 17)
     {
@@ -50,17 +53,21 @@ int main()
 {
     float fMassa;
     float fAltura;
-
+    // Recebe o input de massa do usuário.
     cout << "Insira sua massa: ";
     cin >> fMassa;
 
+    // Recebe o input de altura do usuário.
     cout << "Insira sua altura: ";
     cin >> fAltura;
 
+    // Calcula o IMC com os inputs.
     float fImc = fCalcularImc(fMassa, fAltura);
 
+    // Verifica a classificação do IMC.
     string sResultado = sVerificarImc(fImc);
 
+    // Imprime no terminal o IMC e a classificação.
     cout << "Seu IMC é: " << fImc << "\n" << "Classificação: " << sResultado;
 
     return 0;
